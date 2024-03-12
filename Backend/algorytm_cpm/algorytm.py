d026d35bf34068dd0eb99795e684a72903e4b41a
Trzciano <krzysiot44@gmail.com>
2024-03-12 18:36:24 +0100

    gantt chart asap/alap

--- Backend/algorytm_cpm/algorytm.py
+++ Backend/algorytm_cpm/algorytm.py
@@ -66,3 +66,29 @@ for task in tasks:
     #print(task.late_finish)
     #print(task.reserve)
     print(task.action,"\t ",task.duration,"\t ",task.early_start, "\t ",task.early_finish,"\t ",task.late_start,"\t ",task.late_finish, "\t ", task.reserve, "\t ", task.is_critical)
+
+##################################  GANTT CHART  ##################################
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, ax = plt.subplots()
+
+for i, task in enumerate(tasks):
+    #Harmonogram ASAP
+    ax.broken_barh([(task.early_start, task.duration)], (i*10, 9), facecolors='tab:blue')
+
+    #Harmonogram ALAP
+    ax.broken_barh([(task.late_start, task.duration)], ((i+0.5)*10, 4), facecolors='tab:orange')
+
+ax.set_yticks([i*10+5 for i in range(len(tasks))])
+ax.set_yticklabels([task.action for task in tasks])
+ax.invert_yaxis()
+
+ax.set_xlabel('Czas')
+ax.set_xticks(np.arange(0, max(task.late_finish for task in tasks)+1, 1))
+
+ax.set_title('GANTT CHART')
+ax.legend(['ASAP', 'ALAP'])
+
+plt.show()
